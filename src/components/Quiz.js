070de6c011083bbafaf81c273765c1dc1935f7b0
Wafiy HM <wafiy@anak.it>
2024-08-20 14:46:57 +0800

    added classname and import css to index.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,9 +2,13 @@ import Question from "./Question";
 
 const Quiz = () => {
     return (
-        <div>
-            <div><h1>Quiz</h1></div>
-            <Question></Question>
+        <div className="quiz">
+            <div>
+                <div className="score">
+                    Question 1/8
+                </div>
+                <Question></Question>
+            </div>
         </div>
       );
 };
