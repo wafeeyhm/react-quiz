b27573f2a414510e9d15cd923a847bec3b847ced
Wafiy HM <wafiy@anak.it>
2024-08-20 15:15:58 +0800

    not a good practice and i'm not sure

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,11 +3,18 @@ import Question from "./Question";
 
 const Quiz = () => {
 
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [state, setState] = useState({
+        currentQuestionIndex: 0,
+        questions: [],
+    });
 
     const testClick = () => {
         console.log("next button have been clicked");
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        //setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setState({
+            ...state,
+            currentQuestionIndex: state.currentQuestionIndex + 1,
+        });
     };
 
     return (
@@ -17,7 +24,7 @@ const Quiz = () => {
                     Question 1/8
                 </div>
                 <Question></Question>
-                <div className="next-button" onClick={testClick}>Next {currentQuestionIndex}</div>
+                <div className="next-button" onClick={testClick}>Next {state.currentQuestionIndex}</div>
             </div>
         </div>
       );
